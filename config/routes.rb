90101bc2e0fd8fd3647a87133512c50e9eb01d1d
gorann1 <gorandevop@outlook.com>
2023-01-25 17:45:37 +0100

    #administrate_success

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :zones
+
+      root to: "zones#index"
+    end
   resources :zones
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
